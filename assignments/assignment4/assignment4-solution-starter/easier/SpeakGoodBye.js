d643bc56c58c3ff680030785c9699c5872d0c0b2
Yoseph legesse <jossylegesse27@gmail.com>
2025-04-08 08:18:29 -0900

    Update SpeakGoodBye.js

--- assignments/assignment4/assignment4-solution-starter/easier/SpeakGoodBye.js
+++ assignments/assignment4/assignment4-solution-starter/easier/SpeakGoodBye.js
@@ -19,6 +19,7 @@ var speakWord = "Good Bye";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
+speak(yoseph);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
